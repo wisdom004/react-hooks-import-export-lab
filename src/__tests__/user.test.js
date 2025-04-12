799b8bb5edad1cf0d2bee5dfa5da92ff83a42f18
ian <ianwisdom90@gmail.com>
2025-04-12 17:37:49 +0300

    export/import

--- src/__tests__/user.test.js
+++ src/__tests__/user.test.js
@@ -5,3 +5,5 @@ test("username, city, and image are all exported", () => {
   expect(city).toEqual("New York");
   expect(image).toEqual("https://i.imgur.com/mV8PQxj.gif");
 });
+
+export { username, city, image };
